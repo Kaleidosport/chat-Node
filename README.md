1a84229501f44a9c057b01d0742bf1df2dc873f8
Kaleidosport <lambert.ant14@gmail.com>
2021-12-23 17:06:07 +0100

    Temporary

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# chat-Node
-Real time chat app.
+<h1 align="center">RAD.io, a manganime chat app</h1>  
+<div align="center"><img src="/Views/Images/logo.svg" width="400px"/></div>  
+<div align="center"><img src="/Views/Images/nodejs-badge.svg"/> <img src="/Views/Images/socketio-badge.svg"/> <img src="/Views/Images/team-badge.svg"/></div>  
+  
+## About  
+                          
+**Contributors:** [Anthony Lambert](https://github.com/Kaleidosport), [Romain Berthaux](https://github.com/kawtagan) and [Jean-Louis Houdret](https://github.com/houdret).  
+**Subject:** Concluding group assignment within the scope of the Junior WebDev's training provided by [BeCode](https://github.com/becodeorg), NodeJS's latest bundle.  
+**Start date:** December 17th 2021.  **End date:** December 24th 2021?  
+**Self-hosted release:** Coming soon.     
+  
+## Learning objectives  
+> Working together toward a common vision.
+  
+* To create a real time chat app while making use of NodeJS, Socket.io and ExpressJS 
+* To find tricks and workarounds so that the aforementioned packages work just fine together 
+* To divide our workload in the smartest way to avoid conflicts  
+* To shape the whole project while complying with a subtle list of requirements    
+* To make sure the whole project hinged on a proper MVC structure, though it wasn't mandatory 
+* To browse through a hella lot of docs 
+* To get accustomed to bigger-scale projects      
+  
+---  
+
+## Work flow  
+> Rinse and repeat.  
+  
+### Getting a headstart  
+
+---          
+    
+## Have a break, have a chit-chat!  
+> Merry Christmas, y'all!    
+  
+![Filthy animals](https://64.media.tumblr.com/1494c96c178cdf6b6f675314d3e9e5fc/tumblr_p1h86k75wK1uzomqmo1_500.gif)  
--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const MONGOOSE = require("mongoose")
 const EXPRESS = require("express")
-const SOCKET = require("socket.io")
+const { Server } = require("socket.io")
+const MESSAGE = require("./Models/messages")
 
 require("dotenv").config()
 
@@ -11,17 +12,60 @@ const MONGO_PW = process.env.MONGO_PW
 const MONGO_DB = process.env.MONGO_DB
 
 const APP = EXPRESS()
+const SERVER = require("http").createServer(APP)
+const IO = new Server(SERVER)
 
-// require("./Models/users")(APP)
+require("./Models/users")(APP)
 
 MONGOOSE.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PW}@${MONGO_DB}.5c5eb.mongodb.net/rtc?retryWrites=true&w=majority`)
+        .then(() => console.log(`Authentication successful.`))
+        .catch(error => console.error(`Unexpected error.`, error))
 
 const PORT = 5000
 
+APP.use("/Views", EXPRESS.static("./Views"))
 APP.get("/", (req, res) => {
-    res.send("Hello world!")
+    res.sendFile(__dirname + "/Views/index.html")
 })
 
-APP.listen(PORT, () => {
+let nicknames = []
+
+IO.on("connection", socket => {
+    console.log(`User connected.`)
+    MESSAGE.find({})
+           .sort({createdAt: -1})
+           .limit(10)
+           .then(messages => socket.emit(`Load previous messages`, messages.reverse())
+           ) // Oddly enough, reverse() was key to get the same order on FireFox & Chrome...
+    socket.on(`New user`, data => {
+        socket.nickname = data
+        console.log(data)
+        nicknames.push(socket.nickname)
+        console.log(nicknames)
+        IO.sockets.emit(`usernames`, nicknames)
+    })
+    socket.on(`Chat message`, msg => {
+        const MESSAGES = new MESSAGE({message: msg})
+        MESSAGES.save().then(() => IO.emit(`Chat message`, msg) 
+        )        
+    })
+    socket.on(`disconnect`, () => {
+        console.log(`User disconnected.`)
+    })
+})
+
+SERVER.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}.`)
 })
+
+/* Where we at: 
+ *
+ * https://expressjs.com/en/starter/hello-world.html
+ * https://www.npmjs.com/package/mongoose
+ * https://nodejs.org/en/docs/
+ * https://socket.io/get-started/chat/
+ * https://docs.mongodb.com/
+ * 
+ * Page 371
+ * 
+ */
